57cab537f67840a1dbda26008afa87ff521c8f8b
Blindbusy <f2414227968@gmail.com>
2025-02-06 21:11:19 +0800

    uni-ui组件库

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,8 +4,10 @@
 
 <template>
   <view class="index">index</view>
+  <uni-card title="基础卡片" subtitle="副标题" extra="额外信息" @click="">
+    <text>带有头像和双标题的基础卡片示例</text>
+  </uni-card>
 </template>
 
 <style lang="scss">
-//
-</style>
+//</style>
