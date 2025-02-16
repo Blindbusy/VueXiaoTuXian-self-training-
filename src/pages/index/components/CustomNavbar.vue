abd98295aad9a9f625862f958260f1af98538c93
Blindbusy <f2414227968@gmail.com>
2025-02-16 22:28:58 +0800

    完成猜你喜欢的基础渲染和分页数据类型、猜你喜欢范型的定义

--- src/pages/index/components/CustomNavbar.vue
+++ src/pages/index/components/CustomNavbar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 //获取屏幕边界到安全区域的距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
-// console.log(safeAreaInsets)
+console.log('屏幕上沿空白：', safeAreaInsets)
 </script>
 
 <template>
