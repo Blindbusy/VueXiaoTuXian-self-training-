ed346c17340be4391259204c6a713cf141583c86
Blindbusy <f2414227968@gmail.com>
2025-02-20 14:36:33 +0800

    商品详情的页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,8 +1,9 @@
 import { http } from '@/utils/http'
+import type { GoodsResult } from '@/types/goods'
 
 // 商品详情
 export const getGoodsByIdAPI=(id:string)=>{
-  return http({
+  return http<GoodsResult>({
     method:"GET",
     url:'/goods',
     data:{
