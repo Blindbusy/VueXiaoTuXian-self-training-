4442d8192927ace0c8078d0af92505d797d18561
Blindbusy <f2414227968@gmail.com>
2025-02-22 21:47:36 +0800

    手机号快捷登录和模拟登陆

---
+++ src/services/login.ts
@@ -0,0 +1,31 @@
+import {http} from "@/utils/http"
+
+type LoginParams={
+  code:string,
+  encrypteData:string,
+  iv:string
+}
+
+// 小程序登陆
+export const postLoginWxMinAPI=(data:LoginParams)=>{
+  return http({
+    method:"POST",
+    url:"/login/wxMin",
+    data
+  })
+}
+
+// 小程序登陆-内测版
+/**
+ * 小程序登录_内测版
+ * @param phoneNumber 模拟手机号码
+ */
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber,
+    },
+  })
+}
