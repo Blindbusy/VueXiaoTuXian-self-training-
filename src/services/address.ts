1c987d758e56c6cb9b23f7cb1e21aed10c65d375
Blindbusy <f2414227968@gmail.com>
2025-02-26 22:16:29 +0800

    地址模块-列表渲染

--- src/services/address.ts
+++ src/services/address.ts
@@ -1,5 +1,6 @@
 import {http} from "@/utils/http"
 import type {AddressParams} from "@/types/address.d.ts"
+import type { AddressItem } from '@/types/address.d'
 
 // 添加收获地址
 export const postMemberAddressAPI = (data:AddressParams)=>{
@@ -7,5 +8,13 @@ export const postMemberAddressAPI = (data:AddressParams)=>{
     method:'POST',
     url:'/member/address',
     data
+  })
+}
+
+// 获取收货地址列表
+export const getMemberAddressAPI=()=>{
+  return http<AddressItem[]>({
+    method:'GET',
+    url:"/member/address"
   })
 }
